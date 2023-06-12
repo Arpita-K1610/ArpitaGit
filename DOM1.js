76a2712f3d572e946ae0a7e41d97bf91ab858567
Arpita-K1610 <126654567+Arpita-K1610@users.noreply.github.com>
2023-06-12 12:31:10 +0530

    Add files via upload

---
+++ DOM1.js
@@ -0,0 +1,9 @@
+localStorage.setItem('name', 'Arpita');
+///localStorage.removeItem('name')
+
+//sessionStorage
+
+sessionStorage.setItem('name', 'suhani');
+console.log(sessionStorage.getItem('name'));
+
+//sessionStorage.removeItem('name');
