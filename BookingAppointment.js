74adbcc95d4dc396241084593eca3dc4589d8b4b
Arpita-K1610 <126654567+Arpita-K1610@users.noreply.github.com>
2023-07-25 23:42:40 +0530

    Add files via upload

--- BookingAppointment.js
+++ BookingAppointment.js
@@ -1,26 +1,67 @@
-const http=require('http');
+const express = require('express');
+const mysql = require('mysql2');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-const express=require('express');
-const { Console } = require('console');
+const app = express();
+const port = 3001;
 
-const app=express();
+app.use(bodyParser.json());
+app.use(cors());
 
+//Create a MySQL connection
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'Niteesh@1995',
+  database: 'BookingAppointment',
+});
 
+// Connect to the database
+db.connect((err) => {
+  if (err) throw err;
+  console.log('Connected to the database.');
+});
 
-app.use((req,res,next)=>{
-    console.log("i'M A first MIDDLEWERE");
-    next() // this is midleware for allow the request for next midleware line
-})
-app.use((req,res,next)=>{
-    console.log("i'M A second MIDDLEWERE");
-    res.send(<h1> HI, this is my express js code </h1>)
+// API routes
+app.post('/api/users', (req, res) => {
+  const { name, email, phone } = req.body;
+  const query = 'INSERT INTO users (name, email, phone) VALUES (?, ?, ?)';
+  db.query(query, [name, email, phone], (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(500).json({ error: 'Error inserting user.' });
+    } else {
+      res.status(201).json({ message: 'User inserted successfully.' });
+    }
+  });
+});
 
-})
-app.use((req,res,next)=>{
-    console.log("i'M A third MIDDLEWERE");
+app.get('/api/users', (req, res) => {
+  const query = 'SELECT * FROM users';
+  db.query(query, (err, results) => {
+    if (err) {
+      console.log(err);
+      res.status(500).json({ error: 'Error fetching users.' });
+    } else {
+      res.json(results);
+    }
+  });
+});
 
-})
+app.delete('/api/users/:id', (req, res) => {
+  const userId = req.params.id;
+  const query = 'DELETE FROM users WHERE id = ?';
+  db.query(query, [userId], (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(500).json({ error: 'Error deleting user.' });
+    } else {
+      res.json({ message: 'User deleted successfully.' });
+    }
+  });
+});
 
-const server= http.createServer(app);
-
-server.listen(8080);
+app.listen(port, () => {
+  console.log(`Server running on port http://localhost:${port}`);
+});
